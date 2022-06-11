2aa1ba90e33da6a19213c064a94dd4116a2ddd02
Gers2017 <Gers2017@outlook.com>
2022-06-11 10:09:20 -0600

    Update day-17

--- src/day-17-rs/src/main.rs
+++ src/day-17-rs/src/main.rs
@@ -1,23 +1,18 @@
-use std::fs;
 use std::collections::HashSet;
+use std::{fs, io, num};
 
-fn main() {
-    let input = get_input(false);
+fn main() -> Result<(), Error> {
+    let input = get_input(Mode::File)?;
     println!("Using {}", input);
-    let some_area = Area::from(input);
-    if some_area.as_ref().is_none() {
-        println!("Invalid input");
-        return;
-    }
+    let area = Area::new(input)?;
 
-    let area = some_area.unwrap();
-    let mut velocities: Vec<Vector> = vec![];
+    let max_y = area.max_y().clone() - 50;
+    let max_x = area.max_x().clone() + 50;
 
-    for y in area.get_max_y() - 50..area.get_max_x() {
-        for x in 0..area.get_max_x() + 50 {
-            velocities.push(Vector::new(x, y));
-        }
-    }
+    let velocities = (max_y..max_x)
+        .map(move |y| (0..max_x).map(move |x| Vector::new(x, y)))
+        .flatten()
+        .collect::<Vec<_>>();
 
     let mut launcher = Launcher::new(area);
 
@@ -28,16 +23,29 @@ fn main() {
 
     println!("Highest Y: {}", launcher.get_highest_y());
     println!("Velocities:{}", launcher.get_velocities_count());
+    Ok(())
 }
 
-fn get_input(is_test: bool) -> String {
-    if is_test {
-        return "target area: x=20..30, y=-10..-5".to_string();
+#[derive(Debug)]
+enum Error {
+    Input(io::Error),
+    ParseArea(num::ParseIntError),
+    InvalidArea,
+}
+enum Mode {
+    #[allow(dead_code)]
+    Test,
+    #[allow(dead_code)]
+    File,
+}
+fn get_input(mode: Mode) -> Result<String, Error> {
+    match mode {
+        Mode::Test => Ok("target area: x=20..30, y=-10..-5".to_string()),
+        Mode::File => fs::read_to_string("input.txt").map_err(Error::Input),
     }
-    return fs::read_to_string("input.txt").unwrap();
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 struct Vector {
     x: i16,
     y: i16,
@@ -45,23 +53,20 @@ struct Vector {
 
 impl std::fmt::Display for Vector {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.as_string())
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+impl std::ops::AddAssign for Vector {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
     }
 }
 
 impl Vector {
-    fn new(x: i16, y: i16) -> Vector{
-        Vector{ x, y }
-    }
-
-    fn add(&self, b: &Vector) -> Vector {
-        let x = self.x as i16;
-        let y = self.y as i16;
-        return Vector::new(x + b.x, y + b.y);
-    }
-
-    fn as_string(&self) -> String {
-        return format!("x: {} y: {}", self.x, self. y).to_string();
+    fn new(x: i16, y: i16) -> Vector {
+        Vector { x, y }
     }
 
     fn zero() -> Vector {
@@ -75,28 +80,27 @@ struct Probe {
 }
 
 impl Probe {
-    fn new(v: Vector) -> Probe {
-        Probe { position: Vector::zero(), velocity: v }
+    fn new(velocity: Vector) -> Probe {
+        Probe {
+            position: Vector::zero(),
+            velocity,
+        }
     }
 
-    fn restart(&mut self, v: Vector) {
-        self.velocity = v;
+    fn restart(&mut self, velocity: Vector) {
+        self.velocity = velocity;
         self.position = Vector::zero();
     }
 
     fn apply_velocity(&mut self) {
-        let velocity = self.velocity;
-        self.position = self.position.add(&velocity);
+        self.position += self.velocity;
     }
 
-    
     fn apply_drag(&mut self) {
-        if self.velocity.x != 0 {
-            let drag = self.velocity.x.signum() * -1;
-            self.velocity.x += drag;
-        }
+        let drag = self.velocity.x.signum();
+        self.velocity.x -= drag;
     }
-    
+
     fn apply_gravity(&mut self) {
         self.velocity.y -= 1;
     }
@@ -110,43 +114,52 @@ impl Probe {
 
 struct Area {
     start: Vector,
-    end: Vector
+    end: Vector,
 }
 
 impl Area {
-    fn from(line: String) -> Option<Area>{
+    fn new(line: String) -> Result<Area, Error> {
         if !line.starts_with("target area:") {
-            return None;
+            return Err(Error::InvalidArea);
         }
 
-        let ranges= line.replace("target area: ", "")
-            .trim().to_owned()
+        let ranges: Vec<Vec<i16>> = line
+            .replace("target area:", "")
+            .trim()
             .split(", ")
             .map(|x| {
-                x[2..].split("..")
-                    .filter_map(|s| s.parse::<i16>().ok())
-                    .collect::<Vec<i16>>()
+                x[2..]
+                    .split("..")
+                    .map(|s| s.parse().map_err(Error::ParseArea))
+                    .collect::<Result<_, _>>()
             })
-            .collect::<Vec<_>>();
+            .collect::<Result<_, _>>()?;
 
         let range_x = &ranges[0];
         let range_y = &ranges[1];
 
-        return Some(Area {
+        Ok(Area {
             start: Vector::new(range_x[0], range_y[0]),
-            end: Vector::new(range_x[1], range_y[1])
-        });
+            end: Vector::new(range_x[1], range_y[1]),
+        })
     }
 
-    fn get_max_x(&self) -> i16 { self.end.x }
-    fn get_max_y(&self) -> i16 { self.end.y }
-    fn get_min_x(&self) -> i16 { self.start.x }
-    fn get_min_y(&self) -> i16 { self.start.y }
+    fn max_x(&self) -> i16 {
+        self.end.x
+    }
+    fn max_y(&self) -> i16 {
+        self.end.y
+    }
+    fn min_x(&self) -> i16 {
+        self.start.x
+    }
+    fn min_y(&self) -> i16 {
+        self.start.y
+    }
 
     fn contains(&self, point: Vector) -> bool {
-        return 
-            self.get_min_x() <= point.x && point.x <= self.get_max_x()
-            && self.get_min_y() <= point.y && point.y <= self.get_max_y();
+        (self.min_x()..=self.max_x()).contains(&point.x)
+            && (self.min_y()..=self.max_y()).contains(&point.y)
     }
 }
 
@@ -154,12 +167,12 @@ struct Launcher {
     probe: Probe,
     area: Area,
     heights: Vec<i16>,
-    velocities: HashSet<String>,
+    velocities: HashSet<Vector>,
 }
 
 impl Launcher {
     fn new(area: Area) -> Launcher {
-        return Launcher{
+        return Launcher {
             area,
             probe: Probe::new(Vector::zero()),
             heights: vec![],
@@ -168,49 +181,39 @@ impl Launcher {
     }
 
     fn get_highest_y(&mut self) -> i16 {
-        self.heights.sort_by(|a, b| b.cmp(a));
-        return self.heights.get(0)
-            .map_or(0, |x| x.to_owned());
+        self.heights.iter().copied().max().unwrap_or(0)
     }
 
     fn get_velocities_count(&self) -> usize {
         return self.velocities.len();
     }
 
-    fn setup(&mut self, v: Vector) {
-        self.probe.restart(v);
+    fn setup(&mut self, velocity: Vector) {
+        self.probe.restart(velocity);
     }
 
     fn is_missing_shot(&self, point: Vector) -> bool {
-        return point.x > self.area.get_max_x() 
-            || point.y < self.area.get_min_y(); // below the lowest point
+        // below the lowest point
+        return point.x > self.area.max_x() || point.y < self.area.min_y();
     }
 
     fn launch(&mut self) {
         let init: Vector = self.probe.velocity;
+        let mut possible_pos: Vec<i16> = vec![];
         // println!("Init: {}", init);
 
-        let mut possible_pos: Vec<i16> = vec![];
-        let mut in_area = self.area.contains(self.probe.position);
+        while !self.area.contains(self.probe.position) {
+            if self.is_missing_shot(self.probe.position) {
+                return;
+            }
 
-        while !in_area && !self.is_missing_shot(self.probe.position) {
             self.probe.simulate();
             possible_pos.push(self.probe.position.y);
-            in_area = self.area.contains(self.probe.position);
             // println!("{}", self.probe.position);
         }
 
-        if in_area {
-            // println!("Final: {}", self.probe.position);
-            self.heights.extend(possible_pos);
-            self.velocities.insert(init.as_string());
-        }
+        self.heights.extend(possible_pos);
+        self.velocities.insert(init);
+        // println!("Final: {}", self.probe.position);
     }
 }
-
-
-
-
-
-
-
