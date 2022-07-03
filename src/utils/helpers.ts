ddc3c76fa90502b5fb99addff8f28d073e53a25e
Gers2017 <Gers2017@outlook.com>
2022-07-02 22:20:18 -0600

    add get_input_raw

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,7 +3,19 @@ export enum InputMode {
     Test = 1,
 }
 
+const real_filename = "input.txt";
+const test_filename = "input_test.txt";
+
+function read_file(filename: string): string {
+    return Deno.readTextFileSync(filename).trim();
+}
+
 export function get_input(mode: InputMode) {
-    const filename = mode == InputMode.Real ? "input.txt" : "input_test.txt";
-    return Deno.readTextFileSync(filename).trimEnd().split("\n");
+    const filename = mode == InputMode.Real ? real_filename : test_filename;
+    return read_file(filename).split("\n");
+}
+
+export function get_input_raw(mode: InputMode) {
+    const filename = mode == InputMode.Real ? real_filename : test_filename;
+    return read_file(filename);
 }
